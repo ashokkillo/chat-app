19bc08d547d8474ed93f0bf4b2925ce3dc4182fe
ashokkillo <ashok.killo@gmail.com>
2020-12-07 23:14:27 +0530

    'dec7-end'

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title={"Give me a Number"}/>
-      
+      <StartGameScreen title={'Game Start'}/>
     </View>
   );
 }
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,14 +1,50 @@
     import React from 'react';
-    import {View, Text, StyleSheet} from 'react-native';
+    import {View, Text, StyleSheet, TextInput,Button} from 'react-native';
 
     const StartGameScreen = props => {
         return (
-            <View>
-                <Text>{props.title}</Text>
+            <View style={styles.screen}>
+                <Text style={styles.title}>{props.title}</Text>
+                <View style={styles.inputContainer}>
+                    <Text>Select a Number</Text>
+                    <TextInput />
+                    <View style={styles.buttonContainer}>
+                        <Button title="Reset" onPress={() => {}} />
+                        <Button title="Confirm" onPress={() => {}} />
+                    </View>
+                </View>
             </View>
         );
     };
-
-    const styles = StyleSheet.create({});
+ 
+    const styles = StyleSheet.create({
+        screen:{
+            flex: 1,
+            padding:10,
+            alignItems: 'center',
+        },
+        title:{
+            fontSize:20,
+            marginVertical:10,
+        },
+        inputContainer:{
+            width: 300,
+            maxWidth: '80%',
+            alignItems: 'center',
+            elevation : 8,
+            backgroundColor: 'white',
+            shadowColor: 'black',
+            padding: 20,
+            borderRadius: 10,
+        },
+        buttonContainer:{
+            flexDirection:'row',
+            width: '100%',
+            justifyContent: 'space-between',
+            paddingHorizontal: 15,
+            
+            
+        },
+    });
 
     export default StartGameScreen
